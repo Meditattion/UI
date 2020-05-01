51725f187ae7370cc3250f8d974860f37dd2173c
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-05-01 19:59:37 +0300

    same color as dummy

--- src/Components/LabelDummyItem/LabelDummyItem.js
+++ src/Components/LabelDummyItem/LabelDummyItem.js
@@ -1,5 +1,5 @@
-import React,{useState} from 'react'
-import { useSelector ,useDispatch } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import actions from '../../Actions'
 
 const getRandomColor = () => {
@@ -13,24 +13,36 @@ const getRandomColor = () => {
 
 
 const LabelDummyItem = (props) => {
-    const [newLabelName,setNewLabelName]=useState('');
+    const [newLabelName, setNewLabelName] = useState('');
+    const [bgColor, setbgColor] = useState(getRandomColor);
     let dispatch = useDispatch();
     const currentLabelCountID = useSelector(state => state.LabelsCounterID);
-    const newDummyLabelIsVisible=useSelector(state=>state.Labels.dummyNewLabel);
+    const newDummyLabelIsVisible = useSelector(state => state.Labels.dummyNewLabel);
 
     return (
-        <div className="main-labels-item" style={{backgroundColor:props.bgColor,
-                                                    display:newDummyLabelIsVisible?'grid':'none' }}>
+        <div className="main-labels-item" style={{
+            backgroundColor: bgColor,
+            display: newDummyLabelIsVisible ? 'grid' : 'none'
+        }}>
             <div className="main-labels-item-blank" >&nbsp;</div>
-            <div style={{ display: "flex", flexDirection: "row" ,paddingLeft:"5px",alignItems:"center",height:"100%"}}>
+            <div style={{ display: "flex", flexDirection: "row", paddingLeft: "5px", alignItems: "center", height: "100%" }}>
                 {/* <span>{props.text}</span> */}
                 <input type="text" placeholder="Enter label's name"
-                      value={newLabelName}  onChange={e=>setNewLabelName(e.target.value)} ></input>
+                    value={newLabelName} onChange={e => setNewLabelName(e.target.value)} ></input>
                 <img alt="" src={process.env.PUBLIC_URL + "/Images/complete.png"}
-                 style={{cursor:"pointer"}} onClick={()=>dispatch(actions.addLabel({text:newLabelName,key:currentLabelCountID,bgColor:getRandomColor()}))}></img>
+                    style={{ cursor: "pointer" }}
+                    onClick={() => {
+                        dispatch(actions.addLabel({ text: newLabelName, key: currentLabelCountID, bgColor: bgColor }))
+                        setNewLabelName('');
+                        setbgColor(getRandomColor);
+                    }}></img>
 
                 <img alt="" src={process.env.PUBLIC_URL + "Images/trashS.svg"}
-                    onClick={() => dispatch(actions.closeDummyLabel())}></img>
+                    onClick={() => {
+                        dispatch(actions.closeDummyLabel());
+                        setNewLabelName('');
+                        setbgColor(getRandomColor);
+                    }}></img>
             </div>
         </div>
     )
