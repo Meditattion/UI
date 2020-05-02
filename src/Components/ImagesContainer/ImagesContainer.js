9d1c3a134d1bf64770b1a2a4fb7e7654e07bb439
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-05-02 13:50:50 +0300

    images clickable and hover

--- src/Components/ImagesContainer/ImagesContainer.js
+++ src/Components/ImagesContainer/ImagesContainer.js
@@ -4,11 +4,10 @@ import ImageItem from '../ImageItem/ImageItem'
 const ImagesContainer = (props) => {
     return (
         <div className="main-images-container">
-        <ImageItem source="Images/dummy.jpg" completed="true"></ImageItem>
-        <ImageItem source="Images/dummy.jpg" completed="true"></ImageItem>
-        <ImageItem source="Images/dummy.jpg" completed="false"></ImageItem>
-        <ImageItem source="Images/dummy.jpg" completed="false"></ImageItem>
-        <ImageItem source="Images/dummy.jpg" completed="false"></ImageItem>
+        <ImageItem source="Images/bounding boxes/9691.png" completed="true"></ImageItem>
+        <ImageItem source="Images/bounding boxes/9941.png" completed="true"></ImageItem>
+        <ImageItem source="Images/bounding boxes/11387.png" completed="false"></ImageItem>
+        <ImageItem source="Images/bounding boxes/34967.png" completed="false"></ImageItem>
     </div>
     )
 }
