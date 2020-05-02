42b07423bf2c801bbc0a92c3e0595b9c226e3838
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-05-02 16:50:25 +0300

    add photo

--- src/Components/ImageItem/ImageItem.js
+++ src/Components/ImageItem/ImageItem.js
@@ -5,6 +5,7 @@ const ImageItem = (props) => {
     return (
         <div className="main-images-item">
             <img completed={props.completed} alt="" src={process.env.PUBLIC_URL + props.source}></img>
+            <div>file name</div>
             {props.completed==="true" && <Completed></Completed>}
         </div>
     )
--- src/Components/ImagesContainer/ImagesContainer.js
+++ src/Components/ImagesContainer/ImagesContainer.js
@@ -19,20 +19,26 @@ const ImagesContainer = (props) => {
 
     useEffect(() => () => {
         // Make sure to revoke the data uris to avoid memory leaks
-        console.log("the files:",files)
+        console.log("the files:", files)
         files.forEach(file => URL.revokeObjectURL(file.preview));
     }, [files]);
     return (
         <div className="main-images-container">
-            <div {...getRootProps({ className: 'dropzone' })}>
+            <div {...getRootProps({ className: 'main-images-dropZone' })}>
                 <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
+                {/* <p>Drag 'n' drop some files here, or click to select files</p> */}
+                <span>Add new image/json</span>
+                <img alt="" src={process.env.PUBLIC_URL + "Images/addPhotoS.svg"}></img>
+
+            </div>
+            <div>
+                {thumbs}
             </div>
             {/* <ImageItem source="Images/bounding boxes/9691.png" completed="true"></ImageItem>
         <ImageItem source="Images/bounding boxes/9941.png" completed="true"></ImageItem>
         <ImageItem source="Images/bounding boxes/11387.png" completed="false"></ImageItem>
         <ImageItem source="Images/bounding boxes/34967.png" completed="false"></ImageItem> */}
-            {thumbs}
+
         </div>
     )
 }
