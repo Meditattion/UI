caa4deb5bd7a1f9d59c64859366477dacceb87b6
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-05-10 14:32:33 +0300

    update old redux

--- src/Reducers/Images.js
+++ src/Reducers/Images.js
@@ -1,14 +1,19 @@
 const initState = {
-    container: []
+    container: [],
+    currentImage:""
 };
 
 
 const Images = (state = initState, action) => {
     switch (action.type) {
+
         case "LOAD_IMAGES":
             return {
                 container: state.container.concat(action.images)
-            }
+            };
+        case "IMAGE_CHANGE":
+            return Object.assign({}, state, {
+                currentImage: action.imageName });
         default:
             return state
     }
