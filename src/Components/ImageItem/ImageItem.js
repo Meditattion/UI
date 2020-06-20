e1543a169f7cf944cf4a8f4f3c91aaa4298db27b
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-06-20 16:35:26 +0300

    selected image focus

--- src/Components/ImageItem/ImageItem.js
+++ src/Components/ImageItem/ImageItem.js
@@ -6,14 +6,19 @@ import actions from '../../Actions'
 const ImageItem = (props) => {
     const dispatch=useDispatch();
     const currentSelector=useSelector(state=>state.Tools.currentSelector);
-    const currentImage=useSelector(state=>state.Tools[currentSelector].currentImage);
+    // const currentImage=useSelector(state=>state.Tools[currentSelector].currentImage);
     const thisImageName=props.name.substring(0,props.name.lastIndexOf('.'));
 
     return (
-        <div className="main-images-item" onClick={()=>{
-            if(thisImageName!==currentImage){
+        <div className={props.isSelected? "main-images-item selectedImage":"main-images-item"} onClick={()=>{
+            // if(thisImageName!==currentImage){
+            console.log(`currentImage ${props.currentImage}`);
+            console.log(`thisImage ${thisImageName}`);
+                if(!props.isSelected){
                 dispatch(actions.imageChange(thisImageName));
-            }
+            }else{
+                    console.log('image is Selected!!')
+                }
         }}>
             <img completed={props.completed} alt="" src={process.env.PUBLIC_URL + props.source}></img>
             <div style={{alignSelf:"center",marginTop:"2px"}}>{props.name}</div>
